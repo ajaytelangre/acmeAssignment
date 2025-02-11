78e8d56bfbc99125cead47c1fae4ebf541040561
ajaytelangre <ajaytelangre11@gmail.com>
2025-02-11 23:23:02 +0530

    inital commit

---
+++ src/main/java/com/acme/demo/controllers/SecreteSantaController.java
@@ -0,0 +1,15 @@
+package com.acme.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecreteSantaController {
+	
+	@PostMapping("uploadEmpList")
+	public void generatSecreteSanta() {
+		
+	}
+
+}
